7c32316a63ca1470c366ee0568c6ada3f41d395b
Anthony <dkominion@yahoo.com>
2017-06-09 22:17:58 +0300

    todo list

---
+++ class/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+	
+	class dbconnect{
+		protected $db_conn;
+		public $db_name = "todo";
+		public $db_user = "administrator";
+		public $db_pass = "todostuffs";
+		public $db_host = "todo.com";
+
+		function connect(){
+			
+		}
+	}
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="icon" type="image/png" href="images/favicon.png">
+		<title>Todo List</title>
+	</head>
+	<body>
+    <?php
+        print "hello";
+    ?>
+	<script src="js/script.js"></script>
+	</body>
+</html>
