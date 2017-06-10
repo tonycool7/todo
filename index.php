65755f49b40a3432c80a014059de14f29a421d66
Anthony <dkominion@yahoo.com>
2017-06-10 06:36:49 +0300

    todo list

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<link rel="icon" type="image/png" href="images/favicon.png">
+        <link rel="icon" type="image/png" href="images/favicon.png">
 		<title>Todo List</title>
 	</head>
 	<body>
     <?php
-        print "hello";
+        print "fuck mehn";
+        foreach(){
+
+        }
     ?>
 	<script src="js/script.js"></script>
 	</body>
